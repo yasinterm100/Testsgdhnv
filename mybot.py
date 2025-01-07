3cd1ef8ff97e0d4d195d51f1a4a026acad3cbaea
yasinterm100 <ai.site.serching10@gmail.com>
2025-01-07 08:39:11 +0330

    mybot.py

--- mybot.py
+++ mybot.py
@@ -1,9 +1,13 @@
 import telebot
 
-bot = telebot.TeleBot('7638563599:AAHiWrHxxfgAjSmIU8Tgb6BzLwIRPUs8z9s')
+# توکن ربات تلگرام خود را اینجا وارد کنید
+TOKEN = 'توکن_ربات_اینجا'
+bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda message: message.text.lower() == "سلام")
+def reply_to_salam(message):
+    bot.reply_to(message, "خوبی")
 
+# شروع به کار ربات
+print("ربات در حال اجراست...")
 bot.polling()
